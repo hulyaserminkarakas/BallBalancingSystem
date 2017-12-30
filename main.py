6226858cc1cdaefc096cbee9922a0f5e716e8925
hulyaserminkarakas <32978851+hulyaserminkarakas@users.noreply.github.com>
2017-12-31 00:20:26 +0300

    Inıtial Commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import pid
+import pid_camera
+import cv2
+import serial_com as arduino_com
+
+
+def main():
+
+    pid_x = pid.PID_Controller("pid_x.config")
+    pid_y = pid.PID_Controller("pid_y.config")
+
+    camera = pid_camera.PIDCamera()
+
+    mode = 0
+
+    x_center = 400
+    y_center = 300
+
+    REFRESH_PERIOD = 5
+    ESC_SC = 27
+
+    SQUARE_SIDE = 140
+    SQUARE_PERIOD = 250
+
+    if mode == 0:
+
+        while 1:
+
+            camera.calculate_position()
+            pid_x.compute_pid( x_center - camera.pos_x )
+            pid_y.compute_pid( y_center - camera.pos_y )
+
+            arduino_com.set_angles( pid_x.output, pid_y.output )
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                break
+
+    elif mode == 1:
+        while 1:
+            #camera.refresh()
+            pass
+
+    camera.reset()
+    arduino_com.close_connection()
+
+if __name__ == "__main__":
+    main()
---
+++ pid.py
@@ -0,0 +1,66 @@
+import re
+
+
+class PID_Controller(object):
+
+    def __init__(self, file_path=None):
+        if file_path is not None:
+            self.read_file(file_path)
+            self.integral_sum = .0
+            self.l_input = .0
+            return
+
+        self.P = .0
+        self.I = .0
+        self.D = .0
+
+        self.l_input = .0
+        self.integral_sum = .0
+
+        self.output = .0
+        self.period = .0
+
+        self.min_out = .0
+        self.max_out = .0
+
+
+    def read_file(self, file_path):
+        with open(file_path) as file:
+
+            lines = file.readlines()
+
+            self.P = float(re.split('\s+',lines[0])[1])
+            self.I = float(re.split('\s+',lines[1])[1])
+            self.D = float(re.split('\s+',lines[2])[1])
+            self.period = float(re.split('\s+',lines[3])[1])
+            self.max_out = float(re.split('\s+',lines[4])[1])
+            self.min_out = float(re.split('\s+',lines[5])[1])
+
+    def compute_pid(self, error):
+        #proportional = self.P * error
+        #derivative = self.D * (error - self.l_input) / self.period
+        #integral = self.integral_sum + self.I * error * self.period
+        derivative = (error - self.l_input)/self.period
+        integral = self.integral_sum + (error * self.period)
+
+
+        if(integral > 20):
+            integral = 100
+
+        elif(integral<-20):
+            integral = -100
+
+
+        self.integral_sum = integral
+        self.l_input = error
+
+        #result = proportional + integral + derivative
+        result = self.P * error + self.I * integral + self.D * derivative
+        self.output = result
+
+        if result > self.max_out:
+            self.output = self.max_out
+        if result < self.min_out:
+            self.output = self.min_out
+
+
