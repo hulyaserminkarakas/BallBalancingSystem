fd2244ba4cf7edd39638eb2c5a8a7233c6857d8a
Ufuk Umut Şentürk <ufuk.senturk@hacettepe.edu.tr>
2017-12-31 18:09:24 +0300

    Create pid_plot.py

---
+++ pid_plot.py
@@ -0,0 +1,70 @@
+import time, random
+from collections import deque
+from matplotlib import pyplot as plt
+
+start_time = time.time()
+
+
+class PositionPlot:
+
+    def __init__(self, max_entries=100):
+        plt.rcParams["figure.figsize"] = [12, 9]
+        x_axes = plt.subplot2grid(shape=(2, 2), loc=(0, 0), colspan=2)
+        y_axes = plt.subplot2grid(shape=(2, 2), loc=(1, 0), colspan=2)
+        x_axes.set_xlabel('Time(s)')
+        x_axes.set_ylabel('X-Axis Position')
+        y_axes.set_xlabel('Time(s)')
+        y_axes.set_ylabel('Y-Axis Position')
+
+        self.pos_x = deque(maxlen=max_entries)
+        self.pos_y = deque(maxlen=max_entries)
+        self.ref_pos_x = deque(maxlen=max_entries)
+        self.ref_pos_y = deque(maxlen=max_entries)
+        self.time = deque(maxlen=max_entries)
+
+        self.x_axes = x_axes
+        self.y_axes = y_axes
+
+        self.lineplot_x_pos, = x_axes.plot([], [], label='X Current Pos')
+        self.lineplot_x_ref, = x_axes.plot([], [], 'r', label='Set Point')
+        self.x_axes.legend()
+        self.x_axes.set_autoscaley_on(True)
+
+        self.lineplot_y_pos, = y_axes.plot([], [], label='Y Current Pos')
+        self.lineplot_y_ref, = y_axes.plot([], [], 'r', label='Set Point')
+        self.y_axes.legend()
+        self.y_axes.set_autoscaley_on(True)
+
+    def add(self, pos_x, pos_y, ref_x, ref_y):
+        cur_time = time.time() - start_time
+        self.time.append(cur_time)
+
+        self.ref_pos_x.append(ref_x)
+        self.pos_x.append(pos_x)
+
+        self.ref_pos_y.append(ref_y)
+        self.pos_y.append(pos_y)
+
+        self.lineplot_x_pos.set_data(self.time, self.pos_x)
+        self.lineplot_x_ref.set_data(self.time, self.ref_pos_x)
+
+        self.lineplot_y_pos.set_data(self.time, self.pos_y)
+        self.lineplot_y_ref.set_data(self.time, self.ref_pos_y)
+
+        self.x_axes.relim()
+        self.x_axes.autoscale_view()
+
+        self.y_axes.relim()
+        self.y_axes.autoscale_view()
+
+    @staticmethod
+    def example():
+        plot = PositionPlot()
+        while True:
+            plot.add(random.random() * 600, random.random() * 800, 300, 400)
+            plt.pause(0.001)
+
+
+if __name__ == "__main__":
+    PositionPlot.example()
+
